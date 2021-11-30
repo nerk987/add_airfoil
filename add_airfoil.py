826008be78ea3aee05cae3ddc1032d71d2e9244c
nerk987 <ianhuish6@gmail.com>
2021-11-30 21:37:18 +1100

    Initial version v1.0.0

---
+++ add_airfoil.py
@@ -0,0 +1,103 @@
+bl_info = {
+    "name": "Add Airfoil",
+    "author": "JIan Huish",
+    "version": (1, 00, 0),
+    "blender": (3, 00, 0),
+    "location": "Add Mesh > Add Airfoil",
+    "description": "Read Dat file and create airfoil mesh",
+    "warning": "",
+    "wiki_url": "https://github.com/nerk987/add_airfoil",
+    "tracker_url": "http://github.com/nerk987/add_airfoil/issues",
+    "category": "Add Mesh",
+}
+
+import bpy
+import bmesh
+from bpy_extras.object_utils import AddObjectHelper
+from bpy_extras.io_utils import ImportHelper
+
+from bpy.props import (
+    FloatProperty,
+)
+
+
+def add_airfoil(filename):
+    """
+    This function takes inputs and returns vertex and face arrays.
+    no actual mesh data creation is done here.
+    """
+    verts = []
+    datFile = open(filename, 'r')
+    # print("Dat File", datFile)
+    for line in datFile:
+        try:
+            verts.append([0.0, float(line.split()[0]), float(line.split()[1])]) 
+        except Exception:
+            pass
+    # print("Verts: ", verts)
+    datFile.close()
+
+    return verts
+
+
+class AddAirfoil(bpy.types.Operator, AddObjectHelper, ImportHelper):
+    """Add an airfoil"""
+    bl_idname = "mesh.airfoil_add"
+    bl_label = "Add Airfoil"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        
+         # ImportHelper mixin class uses this
+        filename_ext = ".txt"
+
+        filter_glob: StringProperty(
+            default="*.txt",
+            options={'HIDDEN'},
+            maxlen=255,  # Max internal buffer length, longer would be clamped.
+        )
+
+       # filename = "S:\\Users\\Ian Huish\\Projects\\3D Prints\\Parametric\\Airfoils\\PW1211.dat"
+
+        verts_loc = add_airfoil(self.filepath)
+
+        mesh = bpy.data.meshes.new("Airfoil")
+
+        bm = bmesh.new()
+
+        for v_co in verts_loc:
+            bm.verts.new(v_co)
+
+        bm.verts.ensure_lookup_table()
+        for i in range(len(verts_loc)-1):
+            bm.edges.new([bm.verts[i], bm.verts[i+1]])
+
+        bm.to_mesh(mesh)
+        mesh.update()
+
+        # add the mesh as an object into the scene with this utility module
+        from bpy_extras import object_utils
+        object_utils.object_data_add(context, mesh, operator=self)
+
+        return {'FINISHED'}
+
+
+def menu_func(self, context):
+    self.layout.operator(AddAirfoil.bl_idname, icon='MESH_CUBE')
+
+# Register and add to the "add mesh" menu (required to use F3 search "Add Box" for quick access)
+def register():
+    bpy.utils.register_class(AddAirfoil)
+    bpy.types.VIEW3D_MT_mesh_add.append(menu_func)
+
+
+def unregister():
+    bpy.utils.unregister_class(AddAirfoil)
+    bpy.types.VIEW3D_MT_mesh_add.remove(menu_func)
+
+
+if __name__ == "__main__":
+    register()
+
+    # test call
+    bpy.ops.mesh.airfoil_add()
